c6a74e852c67b8c51d010218bfa79e095d9b0485
Georgi Georgiev <georgiev.georgi93@gmail.com>
2017-02-09 13:07:44 +0100

    basic reminder with notfications

--- main.ts
+++ main.ts
@@ -2,17 +2,26 @@
 
 class BlinkReminder {
 
-  lastestState: string
-
   init (): void {
       chrome.idle.setDetectionInterval(60);
       chrome.idle.onStateChanged.addListener(this.onStateChanged);
+      chrome.alarms.create("reminder_alarm", {periodInMinutes: 20});
+      chrome.alarms.onAlarm.addListener(this.onAlarmFired);
   }
   onStateChanged (newState: string): void {
     let now: Date = new Date();
     let hours: string = now.getHours() > 9 ? now.getHours().toString(): "0" + now.getHours().toString();
     let minutes: string = now.getMinutes() > 9 ? now.getMinutes().toString(): "0" + now.getMinutes().toString();
     console.log("[".concat(hours, ":", minutes, "]", ": state change, new state ", newState));
+    if (newState != "active"){
+      chrome.alarms.clear("reminder_alarm");
+    } else if (newState == "active") {
+      chrome.alarms.create("reminder_alarm", {periodInMinutes: 20});
+    }
+  }
+  onAlarmFired (alarm: chrome.alarms.Alarm){
+    console.log("alarm has fired");
+    chrome.notifications.create("look_away", {type: "basic", title: "Blink Reminder", message: "Look away from the monitor."})
   }
 }
 
